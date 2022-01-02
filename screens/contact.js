10d02714408ab32d2839a403bb71c5ad9ca628b9
Alexander Soudry <soudryalex@gmail.com>
2022-01-02 09:08:18 -0500

    added a formik form and removed email.js

--- screens/contact.js
+++ screens/contact.js
@@ -1,37 +1,82 @@
 import React, {useRef} from 'react';
-import {View, StyleSheet} from "react-native";
-import { Button, Input, Text } from 'react-native-elements';
-import emailjs from '@emailjs/browser';
-import{ init } from '@emailjs/browser';
+import {View, StyleSheet, Text, TextInput, Button} from "react-native";
+import { Formik } from 'formik';
 
+const Contact = () => {
 
-export default class Contact extends React.Component {
+const sendEmail = (subject, name, email, message, e) => {
+   console.log(subject)
 
-    constructor(props) {
-        super(props);
-      }        
-   
-    render() {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_dgmnbm5', form.current, 'user_GAuav2ceeSb7x5sFSsW54')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+}
+
         return (
             <View style={styles.form}>
-                <Text>Wanna Chat?</Text>
-                <Text>Send me a personal email</Text>
-                        <Text>Name</Text>
-                        <Input placeholder='BASIC INPUT' />
-                        <Text>Email</Text>
-                        <Input placeholder='BASIC INPUT' />
-                        <Text>Subject</Text>
-                        <Input placeholder='BASIC INPUT' />
-                        <Text>Message</Text>
-                        <Input placeholder='BASIC INPUT' />
-                 <Button title="Submit"></Button>
-                </View>
+                <Formik
+               
+     initialValues={{ subject: '', name: '', email: '', message: '' }}
+     onSubmit={(values) => sendEmail(values.subject, values.name, values.email, values.message)
+        //  values => console.log(values.subject)
+        }
+   >
+     {
+    (props) => (
+       <View>
+            <TextInput
+            style={styles.input}
+            placeholder='Subject'
+            onChangeText={props.handleChange('subject')}
+            value={props.values.subject}
+            />
+            <TextInput
+            style={styles.input}
+            placeholder='Name'
+            onChangeText={props.handleChange('name')}
+            value={props.values.name}
+            />
+         <TextInput
+           style={styles.input}
+           onChangeText={props.handleChange('email')}
+           placeholder='Email'
+           value={props.values.email}
+         />
+          <TextInput
+         style={styles.input}
+         placeholder='Message'
+        onChangeText={props.handleChange('message')}
+        value={props.values.message}
+                            />
+         <Button style={styles.button} onPress={props.handleSubmit} title="Submit" />
+       </View>
+     )}
+   </Formik>
+            </View>
         )
-    }
 }
 
 const styles = StyleSheet.create({
     form: {
         flexDirection: 'column'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 10,
+        fontSize: 18,
+        borderRadius: 6,
+    },
+    button: {
+        borderWidth: 1,
+        borderColor: 'black',
+        color: "red"
     }
 })
+
+export default Contact
