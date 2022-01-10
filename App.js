789cb43fa150cc54eec3c271fe5d5c91c65cdcc8
Alexander Soudry <soudryalex@gmail.com>
2022-01-10 13:30:24 -0500

    added Drawer navigation with the stack navigation now nesting inside it. Also disabled warning as they are node modules imports.

--- App.js
+++ App.js
@@ -1,50 +1,40 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {LogBox, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-
 import 'react-native-gesture-handler';
-
-import Home from './screens/home';
-
 import Profile from './screens/profile';
-import Contact from './screens/contact';
 import Login from './screens/login';
 import Schedule from './screens/schedule'
-import Review from './screens/review';
+import Stack from './stack';
+
 
 export default function App() {
-
-  const Stack = createDrawerNavigator();
+  LogBox.ignoreAllLogs()
+  const Drawer = createDrawerNavigator();
   return (
-   <NavigationContainer>
-     <Stack.Navigator initialRouteName="Contact">
-       <Stack.Screen
-        name="Home"
-        component={Home}
-       />
-       <Stack.Screen
-       name="Review"
-       component={Review}
-       />
-       <Stack.Screen
-        name="Profile"
-        component={Profile}
-       />
-       <Stack.Screen
-        name="Contact"
-        component={Contact}
-       />
-       <Stack.Screen
-        name="Login"
-        component={Login}
-       />
-       <Stack.Screen
-        name="Schedule"
-        component={Schedule}
-       />
-     </Stack.Navigator>
-   </NavigationContainer>
+    <NavigationContainer>
+    <Drawer.Navigator initialRouteName="Login">
+    <Drawer.Screen
+      name="Stack"
+      component={Stack}
+     //  options={{ title: 'Home', }}
+      options={{ headerShown: false }}
+      />
+     <Drawer.Screen
+       name="Profile"
+       component={Profile}
+      />
+       <Drawer.Screen
+       name="Login"
+       component={Login}
+      />
+      <Drawer.Screen
+       name="Schedule"
+       component={Schedule}
+      />
+    </Drawer.Navigator>
+  </NavigationContainer>
   );
 }
 
