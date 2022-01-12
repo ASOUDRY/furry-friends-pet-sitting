d77a202e0446a69bf5b65f6f240fe76be66b267e
Alexander Soudry <soudryalex@gmail.com>
2022-01-11 21:43:00 -0500

    removed placeholder buttons, converted to a functional component, and added several functions to allow the addition of more buttons by filling out a formik form.

--- screens/schedule.js
+++ screens/schedule.js
@@ -1,102 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {GooglePlacesInput} from '../components/address.js'
 import {Calender} from '../components/calender.js'
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
 import { Button, Text} from 'react-native-elements';
 import { firestore } from '../components/firebase';
 import {addDoc, collection} from 'firebase/firestore'
+import { Formik } from 'formik';
+import { AnimalButton } from '../components/animalButton.js';
 
-export default class Schedule extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            location: null,
-            startDate: null,
-            endDate: null,
-            id: null
-        }
-    }
 
-    componentDidMount() {
-        this.setState({
-            id: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed"
-        })
-    }
 
-    render() {
-
+const Schedule = () => {
+    const [location, setLocation] = useState(false);
+    const [startdate, setStartDate] = useState(false);
+    const [enddate, setEndDate] = useState(false);
+    const [id, setID] = useState([]);
+    const [animalList, setList] = useState(['cat', 'dog'])
+   
         let click = (value) => {
-            this.setState({
-                location: value
-            })
+              setLocation(value)       
         }
         let dates = (value1, value2) => {
-            this.setState({
-                startDate: value1,
-                endDate: value2
-            })
+            setStartDate(value1),
+            setEndDate(value2)
         }
 
         let submit = async () => {
             try {
                 const docRef = addDoc(collection(firestore, "users"), {
-                  location: this.state.location,
-                  startDate: this.state.startDate,
-                  endDate: this.state.endDate,
-                  id: this.state.id
+                  location: location,
+                  startDate: startdate,
+                  endDate: enddate,
+                  id: id
                 });
-                console.log("Document written with ID: ", docRef.id);
+                // console.log("Document written with ID: ", docRef.id);
               } catch (e) {
                 console.error("Error adding document: ", e);
               }
         }
 
         return (
-            // map over this somehow
             <View style={styles.services}>
                 <Text style={styles.title}>Where do you live?</Text>
                 <GooglePlacesInput click={click} style={styles.input} />
 
                 <Text>What kind of Pets do you have?</Text>
-                <Button title="Dog"/>
-                <Button
-                 icon={{
-                    name: 'arrow-up',
-                    type: 'font-awesome',
-                    size: 15,
-                    color: 'white',
-                  }}
-                  iconRight
-                />
-                 <Button
-                 icon={{
-                    name: 'arrow-down',
-                    type: 'font-awesome',
-                    size: 15,
-                    color: 'white',
-                  }}
-                  iconRight
-                />
-                <Button title="Cat"/>
-                <Button
-                 icon={{
-                    name: 'arrow-up',
-                    type: 'font-awesome',
-                    size: 15,
-                    color: 'white',
-                  }}
-                  iconRight
-                />
-                 <Button
-                 icon={{
-                    name: 'arrow-down',
-                    type: 'font-awesome',
-                    size: 15,
-                    color: 'white',
-                  }}
-                  iconRight
-                />
-                 <Button title="Add new pet"/>
+                <View style={styles.buttonGroup}>
+                    {  
+                    console.log(animalList),
+                            animalList.map((tag) => {
+                                return (
+                                   <AnimalButton animalz={tag}/>
+                                )
+                            })
+                    }
+                </View>
+               
+                 <Button title="Add new pet" style={styles.new} />
+                 <Formik
+                 initialValues={{pet: ''}}
+                 onSubmit={(values, actions) => {
+                    actions.resetForm();
+                   animalList.push(values.pet)
+                   let newList = animalList
+                   setList([...animalList,values.pet])
+                 }}
+                 >
+                     {
+                         (props) => (
+                            <View
+                            >
+                                 <TextInput
+                                 style={styles.input}
+                                 placeholder='Type of Pet'
+                                 onChangeText={props.handleChange('pet')}
+                                 value={props.values.pet}
+                                 />
+                              <Button style={styles.button} onPress={props.handleSubmit} title="Submit" />
+                            </View> 
+                         )
+                     }
+                 </Formik>
                 <Text>What services do you need?</Text>
 
                 <Text style={styles.title}>Where dates will you need?</Text>
@@ -109,10 +93,12 @@ export default class Schedule extends React.Component {
             </View>   
         )
     }
-}
+
+export default Schedule
 
 const styles = StyleSheet.create({
     services: {
+        display: "flex",
         flexDirection: 'column',
     },
     title: {
@@ -130,5 +116,45 @@ const styles = StyleSheet.create({
         textAlign: "center",
         fontSize: 30,
         fontWeight: "bold"
-    }
+    },
+    buttonGroup: {
+        flexDirection: 'row',
+    },
+    new: {
+        color: 'red',
+        width: "50%"
+    },
+    // animalButton: {
+    //     // color: "red",
+    //     // backgroundColor: "black",
+    //     // height: 50,
+    //     width: "200%",
+    //     margin: 10,
+    //     padding: 10,
+    //     borderStyle: 'solid',
+    //     borderColor: 'black'
+    // },
+    buttonGrouptrue: {
+    //    width: "100%",
+        flexDirection: 'row',
+    },
+    directionalButtons: {
+        flexDirection: 'column',
+        // margin: 10,
+        // padding: 10
+    },
+    input: {
+        height: 50,
+        width: 300,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 10,
+        fontSize: 18,
+        borderRadius: 6,
+    },
+    button: {
+        borderWidth: 1,
+        borderColor: 'black',
+        color: "red"
+    },
 })
