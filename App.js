e8b18fbfd8ff6fa35aa85205838bfa2e40e57f5e
Alexander Soudry <soudryalex@gmail.com>
2022-02-21 20:36:46 -0500

    added authenticationflow to the Drawer navigator.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -7,31 +7,56 @@ import Profile from './screens/profile';
 import Schedule from './screens/schedule'
 import Stack from './screens/stack';
 import Identification from './screens/identification';
+import { auth } from './components/firebase.js'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function App() {
+  const [run, setRun] = React.useState()
+  useEffect(() => {
+    test()
+  }, [])
+  
+  const test = async () => {
+    const value = await AsyncStorage.getItem('@storage_Key')
+   setRun(value)
+  }
+
   const Drawer = createDrawerNavigator();
   return (
     <NavigationContainer>
-    <Drawer.Navigator initialRouteName="Stack">
-    <Drawer.Screen
-      name="Stack"
-      component={Stack}
-      />
-     <Drawer.Screen
-       name="Profile"
-       component={Profile}
-      />
-      <Drawer.Screen
-       name="Schedule"
-       component={Schedule}
-      />
-      <Drawer.Screen
-      name="Identification"
-      component={Identification}
-      options={{headerShown: false, title: "Login"}}
-      />
-    </Drawer.Navigator>
+<Drawer.Navigator>
+    {run ? (
+      // No token found, user isn't signed in
+      <>
+       <Drawer.Screen
+         name="Stack"
+         component={Stack}
+         />
+        <Drawer.Screen
+          name="Profile"
+          component={Profile}
+         />
+         <Drawer.Screen
+          name="Schedule"
+          component={Schedule}
+         />
+      </>  
+    ) : (
+     <>
+       <Drawer.Screen
+         name="Stack"
+         component={Stack}
+         />
+           <Drawer.Screen
+         name="Identification"
+         component={Identification}
+         options={{headerShown: false, title: "Login"}}
+         />
+     </>
+     
+    )}
+  </Drawer.Navigator>
   </NavigationContainer>
   );
 }
