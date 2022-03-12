7f7d88f8a7d24db6a5f62cb0a66e8dc3b78dac44
Alexander Soudry <soudryalex@gmail.com>
2022-03-12 16:19:16 -0500

    headers are now hidden.

--- screens/identification.js
+++ screens/identification.js
@@ -9,21 +9,18 @@ import Login from '../identification/login';
 const Identification = () => {
       const Stack = createStackNavigator();
     return (
-      
             <Stack.Navigator >
             <Stack.Screen
                 name="Login"
                 component={Login}
-                // options={{headerShown: false, title: "Login"}}
+                options={{headerShown: false, title: "Login"}}
             />
              <Stack.Screen
         name="Registration"
         component={Registration}
-        // options={{headerShown: false}}
+        options={{headerShown: false}}
        />
             </Stack.Navigator>
-       
-
     )
 }
 
