160a7766807232875d508455539abb0891b9ffbb
Alexander Soudry <soudryalex@gmail.com>
2022-03-12 16:15:08 -0500

    Headers are no longer hidden.

--- screens/stack.js
+++ screens/stack.js
@@ -4,32 +4,54 @@ import Home from '../stack/home';
 import Contact from '../stack/contact';
 import ReviewPage from '../stack/reviewpage';
 import otherReview from './otherReview';
+import { Button } from 'react-native-elements';
 
 
-const Stack = () => {
+
+const Stack = ({navigation}) => {
       const Stack = createStackNavigator();
     return (
       
-            <Stack.Navigator >
+            <Stack.Navigator 
+            screenOptions={
+              {
+                headerLeft: () => (
+                    <Button 
+                    type="clear"
+                    onPress={() => {
+                      navigation.toggleDrawer();
+                    }}
+                    icon={{
+                      name: "bars",
+                      type: 'font-awesome',
+                      // size: 15,
+                      color: 'dodgerblue',
+                      // backgroundColor: 'white'
+                    }}
+                    />
+                )
+              }
+            }
+            >
             <Stack.Screen
                 name="Home"
                 component={Home}
-                options={{headerShown: false}}
+                // options={}
             />
              <Stack.Screen
         name="Contact"
         component={Contact}
-        options={{headerShown: false}}
+        // options={{headerShown: false}}
        />
          <Stack.Screen
         name="Reviews"
         component={ReviewPage}
-        options={{headerShown: false}}
+        // options={{headerShown: false}}
        />
          <Stack.Screen
         name="OtherReview"
         component={otherReview}
-        options={{headerShown: false}}
+        // options={{headerShown: false}}
        />
             </Stack.Navigator>
        
