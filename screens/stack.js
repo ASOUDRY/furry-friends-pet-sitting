efb11601baeb3931440aa602bd9b753364daef7f
Alexander Soudry <soudryalex@gmail.com>
2022-04-23 08:32:01 -0400

    added reference to reviewform

--- screens/stack.js
+++ screens/stack.js
@@ -3,59 +3,31 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../stack/home';
 import Contact from '../stack/contact';
 import ReviewPage from '../stack/reviewpage';
-import otherReview from './otherReview';
-import { Button } from 'react-native-elements';
-
-
+import Reviewform from '../stack/reviewform';
 
 const Stack = ({navigation}) => {
       const Stack = createStackNavigator();
     return (
-      
             <Stack.Navigator 
-            screenOptions={
-              {
-                headerLeft: () => (
-                    <Button 
-                    type="clear"
-                    onPress={() => {
-                      navigation.toggleDrawer();
-                    }}
-                    icon={{
-                      name: "bars",
-                      type: 'font-awesome',
-                      // size: 15,
-                      color: 'dodgerblue',
-                      // backgroundColor: 'white'
-                    }}
-                    />
-                )
-              }
-            }
+            screenOptions={{ headerShown: false }}
             >
             <Stack.Screen
                 name="Home"
                 component={Home}
-                // options={}
             />
              <Stack.Screen
         name="Contact"
-        component={Contact}
-        // options={{headerShown: false}}
+        component={Contact}   
        />
          <Stack.Screen
         name="Reviews"
-        component={ReviewPage}
-        // options={{headerShown: false}}
+        component={ReviewPage}      
        />
          <Stack.Screen
-        name="OtherReview"
-        component={otherReview}
-        // options={{headerShown: false}}
-       />
+        name="Reviewform"
+        component={Reviewform}
+       />     
             </Stack.Navigator>
-       
-
     )
 }
 
