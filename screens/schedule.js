b0b257cf4cd21660a146a558f11d483eab9b2e36
Alexander Soudry <soudryalex@gmail.com>
2022-04-23 09:46:41 -0400

    split the service button into two components, replaced the broken auto complete component with text input. and added a text input for comments.

--- screens/schedule.js
+++ screens/schedule.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import {GooglePlacesInput} from '../components/address.js'
 import {Calender} from '../components/calender.js'
-import { View, StyleSheet, TextInput } from 'react-native';
-import { Button, Text} from 'react-native-elements';
+import { View, StyleSheet, TextInput, ScrollView, KeyboardAvoidingView, Text, Pressable, Modal } from 'react-native';
+import { Button} from 'react-native-elements';
 import { firestore } from '../components/firebase';
 import {addDoc, collection} from 'firebase/firestore'
 import { Formik } from 'formik';
 import { AnimalButton } from '../components/animalButton.js';
-import { ServiceButton } from '../components/serviceButton.js';
+import { ServiceTypeBtn } from '../components/serviceTypeBtn.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { confirmPasswordReset } from 'firebase/auth';
+import { getReactNativePersistence } from 'firebase/auth/react-native';
 
 const Schedule = () => {
     useEffect(() => {
         getData()
     })
 
+    const [modalVisible, setModalVisible] = useState(false);
     const [location, setLocation] = useState(false);
     const [startdate, setStartDate] = useState(false);
     const [enddate, setEndDate] = useState(false);
@@ -23,6 +23,8 @@ const Schedule = () => {
     const [animalList, setList] = useState(['cat', 'dog'])
     const [service, setService] = useState([]);
     const [test, setTest] = useState(false)
+    const [comment, setComment] = useState(false)
+    const [load, reload] = useState('')
     const [pets, setPets] = useState({
         cat: {
         number: 1 
@@ -32,6 +34,7 @@ const Schedule = () => {
     }
  });
 
+const uri = ['Dog', 'Bowl', 'House']
 
  const getData = async () => {
     try {
@@ -40,28 +43,25 @@ const Schedule = () => {
       if(value !== null) {
        setID(value)
       }
-      else {
-          console.log("no Id")
-      }
     } catch(e) {
       // error reading value
     }
   }
 
-
-
-    let passOver = (prop) => {
+    let addtoList = (prop) => {
    
-     let name = prop[0]
-     let nombre = prop[1]
-    
+     let petName = prop[0]
+     let petNumber = prop[1]
+
+     console.log(pets)
      setPets(prevState => ({
         ...prevState,           // copy all other field/objects
-        [name]: {              // recreate the object that contains the field to update
-          ...prevState.name, // copy all the fields of the object
-          number: nombre    // overwrite the value of the field to update
+        [petName]: {              // recreate the object that contains the field to update
+        //   ...prevState.name, // copy all the fields of the object
+          number: 5    // overwrite the value of the field to update
         }
-      }));
+      })
+      );
  }
 
  const removePets = (target) => {
@@ -70,7 +70,6 @@ const Schedule = () => {
     )
  }
 
- 
 const setType = (state, title) => {
 if (state === false) {
     setService([...service, title])
@@ -83,16 +82,13 @@ else {
 }
 
 const setVisit = (prop) => {
-    console.log(prop)
+    // console.log(prop)
     triggerOpen()
 }
 
     const serviceList = ["Walking", "Drop-In", "House-Sitting"]
-    const datesList = ["One-Time", "Re-Occuring"]
+    // const datesList = ["One-Time", "Re-Occuring"]
    
-    let click = (value) => {
-              setLocation(value)       
-        }
     let dates = (value1, value2) => {
             setStartDate(JSON.stringify(value1)),
             setEndDate(JSON.stringify(value2))
@@ -111,36 +107,87 @@ const setVisit = (prop) => {
                   startDate: startdate,
                   endDate: enddate,
                   id: id,
-                  current: true,
-                  service: service
+                  status: 'future',
+                  service: service,
+                  comment: comment,
+                  update: []
                 });
-                // console.log("Document written with ID: ", docRef.id);
+                setModalVisible(true)
+                alert("Your visit has been submitted")
+                setComment('')
+                setLocation('')
               } catch (e) {
-                console.error("Error adding document: ", e);
+               alert(e)
               }
         }
 
         return (
-            <View style={styles.services}>
-                <Text style={styles.title}>Where do you live?</Text>
-                <GooglePlacesInput click={click} style={styles.input} />
+                  <KeyboardAvoidingView
+    behavior="height"
+    keyboardVerticalOffset={100}
+    >
+         <ScrollView style={styles.services}>
 
-                <Text>What kind of Pets do you have?</Text>
+         <Modal
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View 
+        style={styles.centeredView}
+        >
+          <View 
+          style={styles.modalView}
+          >
+            <Text
+                  style={styles.inputQuestion}
+                >
+                    Your visit has been scheduled.</Text>
+            <Pressable
+              style={styles.buttonStyle}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Close this box.</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+                 <Text style={styles.title}>Book a Visit</Text>
+                <Text 
+                style={styles.inputQuestion}
+                >Where do you live?</Text> 
+                <View>
+              
+                <TextInput
+                 value={location}
+                style={styles.mapInput}
+                placeholder='Enter your address here.'
+                onChangeText={(value) => setLocation(value)}
+                />
+                </View>
+              
+                <Text
+                  style={styles.inputQuestion}
+                >
+                    What kind of Pets do you have?</Text>
                 <View style={styles.buttonGroup}>
                     {  
                             animalList.map((tag) => {
                                 return (
-                                   <AnimalButton remove={removePets} test={passOver}  animalz={tag}/>
+                                   <AnimalButton key={tag} remove={removePets} addtoList={addtoList} 
+                                    animalz={tag} number={pets}         
+                                    />
                                 )
                             })
-                    }
+                    } 
                 </View>
                
-                 <Formik 
-                     
+                  <Formik  
                  initialValues={{pet: ''}}
                  onSubmit={(values, actions) => {
-                     console.log(values.pet)
+             
                 let freshPet = {
                     [values.pet]: {
                         number: 1
@@ -156,6 +203,9 @@ const setVisit = (prop) => {
                      {
                          (props) => (
                             <View
+                            style={{
+                                flexDirection: 'row',
+                            }}
                             >
                                  <TextInput
                                  style={styles.input}
@@ -163,29 +213,43 @@ const setVisit = (prop) => {
                                  onChangeText={props.handleChange('pet')}
                                  value={props.values.pet}
                                  />
-                              <Button style={styles.button} onPress={props.handleSubmit} title="Add a new Pet" />
+                              <Button buttonStyle={styles.addButton} titleStyle={{color: 'black'}} onPress={props.handleSubmit} title="Add a New Pet" />
                             </View> 
                          )
                      }
                  </Formik>
                 
-                <Text>What services do you need?</Text>
+                <Text
+                  style={styles.inputQuestion}
+                >What services do you need?</Text>
                 <View
                 style={styles.servicebutton}
                 >
-                {
-                    serviceList.map((title) => {
-                        return (
-                            <ServiceButton returntype={setType} title={title} />
-                        )
-                    })
-                }
+            
+                   <ServiceTypeBtn returntype={setType} title={serviceList} uri={uri} /> 
                 </View>
 
-                <Text>How many visits do you need.?</Text>
+                <Text
+                  style={styles.inputQuestion}
+                >How many visits do you need.?</Text>
                 <Calender date={date} dates={dates}/>
-                <Button title="Submit" onPress={() => submit() } /> 
-            </View>   
+              <Text
+                style={styles.inputQuestion}
+              >Any Extra Notes for me?</Text>
+          <TextInput
+              value={comment}
+                multiline={true}
+                style={styles.commentInput}
+              
+                onChangeText={(value) => setComment(value)}
+                />
+                <Button
+                buttonStyle={styles.addButton}
+                titleStyle={{color: 'black'}}
+                  title="Submit" onPress={() => submit() }
+                />
+                </ScrollView>   
+                </KeyboardAvoidingView>
         )
     }
 
@@ -195,12 +259,15 @@ const styles = StyleSheet.create({
     services: {
         display: "flex",
         flexDirection: 'column',
+        backgroundColor: '#F2F5EE',
     },
     title: {
-        color: 'red',
-        textAlign: 'center'
+        color: 'black',
+        textAlign: 'center',
+        fontSize: 20,
+        marginBottom: 20,
     },
-    input: {
+    imput: {
         marginTop: 16,
         paddingVertical: 8,
         borderWidth: 4,
@@ -228,21 +295,86 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
     },
     input: {
-        height: 50,
-        width: 300,
+        height: 40,
+        width: 150,
         borderWidth: 1,
         borderColor: '#ddd',
         padding: 10,
         fontSize: 18,
         borderRadius: 6,
+        backgroundColor: 'white',
+        marginRight: 5,
+        marginLeft: 10
     },
-    button: {
-        borderWidth: 1,
-        borderColor: 'black',
-        color: "red"
+    addButton: {
+        backgroundColor: '#BEC3AA',
+        borderRadius: 10,
+        height: 40
     },
     servicebutton: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
+        justifyContent: 'center',
+        // width: 80
+    },
+    mapInput: {
+        width: 405, height: 40,
+        margin: 10,
+        borderWidth: .5,
+        borderColor: '#D3D3D3',
+        borderRadius: 9,
+        backgroundColor: 'white',
+        padding: 10
+    },
+    inputQuestion: {
+        marginLeft: 10,
+        color: 'black',
+        fontWeight: 'bold',
+        // marginBottom: 10
+    },
+    commentInput: {
+        height: 100,
+        backgroundColor: 'white',
+        textAlignVertical: 'top',
+        margin: 10,
+        borderRadius: 20
+       
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "#BEC3AA",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+      height: 120
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    buttonStyle : {
+      height: 30,
+      width: 120,
+      backgroundColor: '#6F7643',
+      borderRadius: 10,
+      marginTop: 20
+    },
+    textStyle: {
+      paddingTop: 5,
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    addButton: {
+      backgroundColor: '#6F7643',
+      borderRadius: 10,
+      marginBottom: 25,
     }
 })
